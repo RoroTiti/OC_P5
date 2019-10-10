ebf0e850f6b2b38306abbe87867ae2b90be0033b
Romaric Thibault <torburma@gmail.com>
2019-10-10 10:05:26 +0200

    Table view for categories list

--- controllers/updater_dialog_controller.py
+++ controllers/updater_dialog_controller.py
@@ -1,6 +1,10 @@
-from PySide2 import QtCore
+import operator
+import typing
+
+import PySide2
+from PySide2.QtCore import QJsonDocument, QAbstractTableModel, Qt, QUrl, SIGNAL
 from PySide2.QtNetwork import QNetworkAccessManager, QNetworkReply, QNetworkRequest
-from PySide2.QtWidgets import QDialog
+from PySide2.QtWidgets import QDialog, QProgressDialog, QHeaderView, QAbstractItemView
 
 from views import updater_dialog
 
@@ -8,25 +12,101 @@ from views import updater_dialog
 class UpdaterDialogController(QDialog):
     def __init__(self):
         super(UpdaterDialogController, self).__init__()
-        self.manager = QNetworkAccessManager()
+        self.progress = QProgressDialog("Récupération des catégories...", "", 0, 0, self)
+        self.progress.setFixedWidth(300)
+
+        # noinspection PyTypeChecker
+        self.progress.setCancelButton(None)
+
+        self.fetch_categories_manager = QNetworkAccessManager()
+        self.fetch_categories_manager.finished.connect(self.handle_api_categories)
+
         self.ui = updater_dialog.Ui_Dialog()
         self.ui.setupUi(self)
-        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+
+        self.setAttribute(Qt.WA_DeleteOnClose)
 
         self.ui.btn_load_list.clicked.connect(self.fetch_categories)
 
     def fetch_categories(self):
-        request = QNetworkRequest(QtCore.QUrl("https://fr.openfoodfacts.org/categories.jon"))
-        self.manager.finished.connect(self.handle_api_categories)
-        self.manager.get(request)
+        self.progress.open()
+
+        request = QNetworkRequest(QUrl("https://fr.openfoodfacts.org/categories.json"))
+        self.fetch_categories_manager.get(request)
 
     def handle_api_categories(self, reply: QNetworkReply):
         er = reply.error()
 
         if er == QNetworkReply.NoError:
             bytes_string = reply.readAll()
-            print(str(bytes_string, 'utf-8'))
+
+            json = QJsonDocument.fromJson(bytes_string)
+            obj = json.object()["tags"]
+
+            categories_gt_5000 = []
+
+            for item in obj:
+                if item["products"] >= 5000:
+                    categories_gt_5000.append(Item(item["name"], item["products"]))
+
+            categories_gt_5000.sort(key=lambda x: x.name)
+
+            table_model = MyTableModel(self, categories_gt_5000, ["Catégorie", "Nombre de produits"])
+
+            width = self.ui.table_categories.width() - 10
+
+            self.ui.table_categories.setModel(table_model)
+            self.ui.table_categories.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+            self.ui.table_categories.setColumnWidth(0, int(width * 0.7))
+            self.ui.table_categories.setColumnWidth(1, int(width * 0.3))
 
         else:
             print("Error occured: ", er)
             print(reply.errorString())
+
+        self.progress.close()
+
+
+class MyTableModel(QAbstractTableModel):
+    def __init__(self, parent, my_list, my_header):
+        QAbstractTableModel.__init__(self, parent)
+        self.my_list: [Item] = my_list
+        self.header = my_header
+
+    def rowCount(self, parent: PySide2.QtCore.QModelIndex = ...) -> int:
+        return len(self.my_list)
+
+    def columnCount(self, parent: PySide2.QtCore.QModelIndex = ...) -> int:
+        return 2
+
+    def data(self, index: PySide2.QtCore.QModelIndex, role: int = ...) -> typing.Any:
+        if not index.isValid():
+            return None
+        elif role != Qt.DisplayRole:
+            return None
+
+        if index.column() == 0:
+            return self.my_list[index.row()].name
+        elif index.column() == 1:
+            return self.my_list[index.row()].products
+
+    def headerData(self, section: int, orientation: PySide2.QtCore.Qt.Orientation, role: int = ...) -> typing.Any:
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self.header[section]
+        return None
+
+    # def sort(self, column: int, order: PySide2.QtCore.Qt.SortOrder = ...):
+    #     # """sort table by given column number col"""
+    #     # self.emit(SIGNAL("layoutAboutToBeChanged()"))
+    #     # self.my_list = sorted(self.my_list, key=operator.itemgetter(column))
+    #     # if order == Qt.DescendingOrder:
+    #     #     self.my_list.reverse()
+    #     # self.emit(SIGNAL("layoutChanged()"))
+    #     pass
+
+
+class Item:
+    def __init__(self, name, products):
+        self.name: str = name
+        self.products: int = products
--- views/updater_dialog.py
+++ views/updater_dialog.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'updater_dialog.ui',
 # licensing of 'updater_dialog.ui' applies.
 #
-# Created: Tue Oct  8 13:31:54 2019
+# Created: Thu Oct 10 09:26:25 2019
 #      by: pyside2-uic  running on PySide2 5.13.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,7 +13,7 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 500)
+        Dialog.resize(500, 600)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -28,23 +28,23 @@ class Ui_Dialog(object):
         self.groupBox.setObjectName("groupBox")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.groupBox)
         self.gridLayout_2.setObjectName("gridLayout_2")
-        self.list_categories = QtWidgets.QListWidget(self.groupBox)
-        self.list_categories.setObjectName("list_categories")
-        self.gridLayout_2.addWidget(self.list_categories, 0, 0, 1, 2)
-        self.listWidget = QtWidgets.QListWidget(self.groupBox)
-        self.listWidget.setObjectName("listWidget")
-        self.gridLayout_2.addWidget(self.listWidget, 3, 0, 1, 2)
-        self.btn_load_list = QtWidgets.QPushButton(self.groupBox)
-        self.btn_load_list.setAutoDefault(False)
-        self.btn_load_list.setObjectName("btn_load_list")
-        self.gridLayout_2.addWidget(self.btn_load_list, 1, 0, 1, 1)
         self.btn_add_category = QtWidgets.QPushButton(self.groupBox)
         self.btn_add_category.setAutoDefault(False)
         self.btn_add_category.setObjectName("btn_add_category")
         self.gridLayout_2.addWidget(self.btn_add_category, 1, 1, 1, 1)
+        self.btn_load_list = QtWidgets.QPushButton(self.groupBox)
+        self.btn_load_list.setAutoDefault(False)
+        self.btn_load_list.setObjectName("btn_load_list")
+        self.gridLayout_2.addWidget(self.btn_load_list, 1, 0, 1, 1)
+        self.listWidget = QtWidgets.QListWidget(self.groupBox)
+        self.listWidget.setObjectName("listWidget")
+        self.gridLayout_2.addWidget(self.listWidget, 3, 0, 1, 2)
         self.label = QtWidgets.QLabel(self.groupBox)
         self.label.setObjectName("label")
         self.gridLayout_2.addWidget(self.label, 2, 0, 1, 2)
+        self.table_categories = QtWidgets.QTableView(self.groupBox)
+        self.table_categories.setObjectName("table_categories")
+        self.gridLayout_2.addWidget(self.table_categories, 0, 0, 1, 2)
         self.gridLayout.addWidget(self.groupBox, 0, 0, 1, 1)
 
         self.retranslateUi(Dialog)
@@ -54,7 +54,7 @@ class Ui_Dialog(object):
         Dialog.setWindowTitle(QtWidgets.QApplication.translate("Dialog", "Mise à jour des données", None, -1))
         self.pushButton.setText(QtWidgets.QApplication.translate("Dialog", "Télécharger les produits", None, -1))
         self.groupBox.setTitle(QtWidgets.QApplication.translate("Dialog", "Catégories", None, -1))
-        self.btn_load_list.setText(QtWidgets.QApplication.translate("Dialog", "Charger la liste", None, -1))
         self.btn_add_category.setText(QtWidgets.QApplication.translate("Dialog", "Ajouter", None, -1))
+        self.btn_load_list.setText(QtWidgets.QApplication.translate("Dialog", "Charger la liste", None, -1))
         self.label.setText(QtWidgets.QApplication.translate("Dialog", "Catégories sélectionnées", None, -1))
 
