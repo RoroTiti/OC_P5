07c87f163adce99cc7763406db28642e73c57af5
Romaric Thibault <torburma@gmail.com>
2019-10-14 11:40:26 +0200

    Done the category selection from all categories

--- controllers/updater_dialog_controller.py
+++ controllers/updater_dialog_controller.py
@@ -1,9 +1,9 @@
-import typing
+from typing import Any
 
 import PySide2
-from PySide2.QtCore import QJsonDocument, QAbstractTableModel, Qt, QUrl, QItemSelection, QSortFilterProxyModel
+from PySide2.QtCore import QJsonDocument, QAbstractTableModel, Qt, QUrl, QSortFilterProxyModel, QModelIndex
 from PySide2.QtNetwork import QNetworkAccessManager, QNetworkReply, QNetworkRequest
-from PySide2.QtWidgets import QDialog, QProgressDialog, QAbstractItemView, QHeaderView
+from PySide2.QtWidgets import QDialog, QProgressDialog, QAbstractItemView, QHeaderView, QAbstractScrollArea
 
 from models.api.Category import Category
 from views import updater_dialog
@@ -27,7 +27,45 @@ class UpdaterDialogController(QDialog):
 
         self.setAttribute(Qt.WA_DeleteOnClose)
 
+        self.all_categories = []
+        self.selected_categories = []
+
         self.ui.btn_load_list.clicked.connect(self.fetch_categories)
+        self.ui.btn_add_category.clicked.connect(self.add_category)
+
+        headers = ["Catégorie", "Nombre de produits"]
+
+        # All categories list initialization
+        self.all_categories_table_model = CategoriesTableModel(
+            self,
+            self.all_categories,
+            headers
+        )
+
+        all_categories_proxy = QSortFilterProxyModel()
+        all_categories_proxy.setSourceModel(self.all_categories_table_model)
+
+        self.ui.table_all_categories.setModel(all_categories_proxy)
+        self.ui.table_all_categories.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.ui.table_all_categories.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.ui.table_all_categories.setSortingEnabled(True)
+        self.ui.table_all_categories.sortByColumn(1, Qt.DescendingOrder)
+
+        # Selected categories list initialization
+        self.selected_categories_table_model = CategoriesTableModel(
+            self,
+            self.selected_categories,
+            headers
+        )
+
+        selected_categories_proxy = QSortFilterProxyModel()
+        selected_categories_proxy.setSourceModel(self.selected_categories_table_model)
+
+        self.ui.table_selected_categories.setModel(selected_categories_proxy)
+        self.ui.table_selected_categories.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.ui.table_selected_categories.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.ui.table_selected_categories.setSortingEnabled(True)
+        self.ui.table_selected_categories.sortByColumn(1, Qt.DescendingOrder)
 
     def fetch_categories(self):
         self.progress.open()
@@ -44,30 +82,18 @@ class UpdaterDialogController(QDialog):
             json = QJsonDocument.fromJson(bytes_string)
             obj = json.object()["tags"]
 
-            categories_gt_5000 = []
+            # categories_gt_5000 = []
+
+            self.all_categories_table_model.beginResetModel()
+
+            self.all_categories.clear()
 
             for item in obj:
                 if item["products"] >= 5000:
-                    categories_gt_5000.append(Category(item["name"], item["products"], item["id"]))
+                    self.all_categories.append(Category(item["name"], item["products"], item["id"]))
 
-            categories_gt_5000.sort(key=lambda x: x.name)
-
-            table_model = CategoriesTableModel(self, categories_gt_5000, ["Catégorie", "Nombre de produits"])
-
-            filter_proxy_model = QSortFilterProxyModel()
-            filter_proxy_model.setSourceModel(table_model)
-            filter_proxy_model.setDynamicSortFilter(False)
-            filter_proxy_model.sort(1, Qt.DescendingOrder)
-
-            self.ui.table_all_categories.setModel(filter_proxy_model)
-            self.ui.table_all_categories.setSelectionBehavior(QAbstractItemView.SelectRows)
-            # self.ui.table_all_categories.setSelectionMode(QAbstractItemView.SingleSelection)
-            self.ui.table_all_categories.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
-            self.ui.table_all_categories.setSortingEnabled(True)
-            self.ui.table_all_categories.sortByColumn(1, Qt.DescendingOrder)
-
-            selection_model = self.ui.table_all_categories.selectionModel()
-            selection_model.selectionChanged.connect(self.on_category_selected)
+            self.all_categories_table_model.endResetModel()
+            self.ui.table_all_categories.resizeColumnsToContents()
 
         else:
             print("Error occured: ", error)
@@ -75,21 +101,31 @@ class UpdaterDialogController(QDialog):
 
         self.progress.close()
 
-    def on_category_selected(self, selected: QItemSelection, deselected: QItemSelection):
-        print("Selected items:")
+    def add_category(self):
+        proxy_model: QSortFilterProxyModel = self.ui.table_all_categories.model()
+        source_model: CategoriesTableModel = proxy_model.sourceModel()
+
+        new_categories = []
+
         for row in self.ui.table_all_categories.selectionModel().selectedRows():
-            # selected_index = self.ui.table_all_categories.selectionModel().currentIndex().row()
-            proxy_model: QSortFilterProxyModel = self.ui.table_all_categories.model()
             model_index = proxy_model.index(row.row(), 0)
-            source_model: CategoriesTableModel = proxy_model.sourceModel()
             source_index = proxy_model.mapToSource(model_index)
-            print(source_model.items_list[source_index.row()].id)
+            new_categories.append(source_model.items_list[source_index.row()])
+
+        self.selected_categories_table_model.beginInsertRows(
+            QModelIndex(),
+            len(self.selected_categories),
+            len(self.selected_categories) + len(new_categories) - 1
+        )
+        self.selected_categories.extend(new_categories)
+        self.selected_categories_table_model.endInsertRows()
+        self.ui.table_selected_categories.resizeColumnsToContents()
 
 
 class CategoriesTableModel(QAbstractTableModel):
     def __init__(self, parent, my_list, my_header):
         QAbstractTableModel.__init__(self, parent)
-        self.items_list: [Category] = my_list
+        self.items_list: [] = my_list
         self.header = my_header
 
     def rowCount(self, parent: PySide2.QtCore.QModelIndex = ...) -> int:
@@ -98,7 +134,7 @@ class CategoriesTableModel(QAbstractTableModel):
     def columnCount(self, parent: PySide2.QtCore.QModelIndex = ...) -> int:
         return 2
 
-    def data(self, index: PySide2.QtCore.QModelIndex, role: int = ...) -> typing.Any:
+    def data(self, index: PySide2.QtCore.QModelIndex, role: int = ...) -> Any:
         if not index.isValid() or role != Qt.DisplayRole:
             return None
 
@@ -107,7 +143,7 @@ class CategoriesTableModel(QAbstractTableModel):
         elif index.column() == 1:
             return self.items_list[index.row()].products
 
-    def headerData(self, section: int, orientation: PySide2.QtCore.Qt.Orientation, role: int = ...) -> typing.Any:
+    def headerData(self, section: int, orientation: PySide2.QtCore.Qt.Orientation, role: int = ...) -> Any:
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self.header[section]
         return None
